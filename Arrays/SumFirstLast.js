b727a9f876bdaf27efd4a58e00647f518e306edc
Svetlin Vatev <svetlinvatev@gmail.com>
2025-04-08 12:22:47 +0300

    Add array manipulation examples in arrays.js, SumFirstLast.js, and Reverse.js

---
+++ Arrays/SumFirstLast.js
@@ -0,0 +1,9 @@
+function solve(arrNumbers) {
+    let first = arrNumbers[0];
+    let last = arrNumbers[arrNumbers.length - 1];
+    console.log(first + last);
+}
+
+solve([20, 30, 40]);
+solve([10, 17, 22, 33]);
+solve([11, 58, 69]);
